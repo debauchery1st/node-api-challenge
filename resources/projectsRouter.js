129d05c3265943291f83f25af546cf6864ca1a49
debauchery1st <trevor.j.martin@gmail.com>
2020-02-14 13:40:19 -0500

    middleware, project_id validation

--- resources/projectsRouter.js
+++ resources/projectsRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const projects = require("../data/helpers/projectModel");
+const mw = require("./projectsMiddleWare");
 
 router = express.Router();
 router.get("/", (req, res) => {
   projects.get().then(lst => res.status(200).json(lst));
+});
+router.get("/:id/actions", mw.validateProjectId, (req, res) => {
+  console.log("projectRouter");
+  res.status(200).json(req.actions);
 });
 
 module.exports = router;
